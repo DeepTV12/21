df7725069f6994635a631a8baf135e2c08db2322
DeepTV12 <mer0gamerbusiness@gmail.com>
2024-10-19 18:57:15 +0300

    Update b.py

--- Airdrop-Toolsbots/a/b.py
+++ Airdrop-Toolsbots/a/b.py
@@ -10,7 +10,7 @@ text = "TRAMA CRYPTO"
 ascii_art = pyfiglet.figlet_format(text, font="speed")
 print(colored(ascii_art, 'cyan'))
 
-print(colored("\nt.me/A_Airdrops\n", 'blue'))
+print(colored("\https://t.me/DeepTV12\n", 'blue'))
 
 
 print(colored("-show sessions> Ctrl +b+s", 'yellow'))
